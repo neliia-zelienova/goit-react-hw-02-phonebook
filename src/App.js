beb13b5604725ff0af295691f183fd31b701b7fc
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-02-28 12:25:08 +0200

    Finished step 1
    Form with Name input, contacts list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: [{ id: uuidv4(), name: "TestName1" }],
+    name: "",
+    phone: ""
+  };
+
+  handleInput = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.state.contacts.push({
+      id: uuidv4(),
+      name: this.state.name,
+    });
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: "",
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>Phonebook</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                onChange={this.handleInput}
+                value={this.state.name}
+              ></input>
+            </label>
+            <label>
+              Phone
+              <input
+                type="text"
+                name="phone"
+                onChange={this.handleInput}
+                value={this.state.name}
+              ></input>
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <h1>Contacts</h1>
+          <ul>
+            {this.state.contacts.map((contact) => (
+              <li key={contact.id}>{contact.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
