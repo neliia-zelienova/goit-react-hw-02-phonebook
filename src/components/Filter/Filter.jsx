0ee3cd4e3ff46d31de8cc072a0eeaf5dfce27af1
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-03-08 12:19:26 +0200

    styles update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,13 +3,13 @@ import styles from "./Filter.module.css";
 
 const Filter = ({ onChange }) => {
   return (
-    <label htmlFor="" className={styles.form_label}>
+    <label htmlFor="" className={styles.form__label}>
       Find contacts by name
       <input
         type="text"
         name="filter"
         onChange={onChange}
-        className={styles.form_input}
+        className={styles.form__input}
       ></input>
     </label>
   );
